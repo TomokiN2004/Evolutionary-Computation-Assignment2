1db0c430be2129eba34c7dc46b153b483d871af6
Nethmi <nethmimal21@gmail.com>
2025-09-24 15:32:00 +0930

    Update Exercise_3 header

---
+++ code/Exercise_3.py
@@ -0,0 +1,11 @@
+"""
+Exercise 3 – Genetic Algorithm
+-----------------------------------------------------------------------------
+
+
+Author:
+    Nethmi Ranathunga (a1895261)
+
+Date:
+    September 2025
+"""
